3ece4d02af4766aca12f6bf863030cf968dd24de
Meteor_Z <3030218083@qq.com>
2023-01-08 17:14:46 +0800

    更新一键启动和停止服务

--- src/main/java/com/lzc/reggie/controller/SetmealController.java
+++ src/main/java/com/lzc/reggie/controller/SetmealController.java
@@ -83,6 +83,7 @@ public class SetmealController
 
     /**
      * 删除套餐
+     *
      * @param ids
      * @return
      */
@@ -91,5 +92,48 @@ public class SetmealController
     {
         setmealService.deleteWithDish(ids);
         return R.success("删除成功!");
+    }
+
+    /**
+     * 使得套餐发生停售
+     *
+     * @param ids
+     * @return
+     */
+    @PostMapping("/status/0")
+    public R<String> stopShopping(@RequestParam List<Long> ids)
+    {
+        log.info(ids.toString());
+        // 查询所有与对应的ids 相匹配的 Setmeal 然后 使得所有状态是1 的查询出来
+        // 查询出来的值全部 状态全部设置为0
+        List<Setmeal> list = setmealService.listByIds(ids);
+        log.info("查询出来的数据大小是{}", list.size());
+        for (int i = 0; i < list.size(); i++)
+        {
+            list.get(i).setStatus(0);
+            System.out.println(list.get(i).toString());
+        }
+        // 更新数据
+        setmealService.updateBatchById(list);
+        return R.success("套餐已全部停售");
+    }
+
+    /**
+     * 将所选的商品进行发售
+     *
+     * @param ids
+     * @return
+     */
+    @PostMapping("/status/1")
+    public R<String> startShopping(@RequestParam List<Long> ids)
+    {
+        log.info(ids.toString());
+        List<Setmeal> list = setmealService.listByIds(ids);
+        for (int i = 0; i < list.size(); i++)
+        {
+            list.get(i).setStatus(1);
+        }
+        setmealService.updateBatchById(list);
+        return R.success("套餐已全部出售");
     }
 }
